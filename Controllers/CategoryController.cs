e1020b41bbb73dd24b0c038346704ed946d33d93
Olatunde Yusuf <123118889+olatundey@users.noreply.github.com>
2023-07-05 03:42:34 +0100

    first commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+using BulkyDemo.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BulkyDemo.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //var objCategoryList = _db.Categories.ToList();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+            //return View(new Category);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                //for diff controller,
+                //RedirectToAction("Index","Category")
+            }
+            return View();
+        }
+   
+
+        public IActionResult Edit(int? id) //id user wants to edit
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb2 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            Category? categoryFromDb3 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+            //return View(new Category);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                //for diff controller,
+                //RedirectToAction("Index","Category")
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id) //id user wants to edit
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+            //return View(new Category);
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                //for diff controller,
+                //RedirectToAction("Index","Category")
+            }
+            return View();
+        }
+
+    }
+}
+
---
+++ Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace BulkyDemo.Models
+{
+	public class Category
+	{
+		[Key]
+		public int Id { get; set;}
+		[Required]
+		[MaxLength(30,ErrorMessage = "The Category Name field is required")]
+		[DisplayName("Category Name")]
+		public string? Name { get; set;}
+        [DisplayName("Display Order")]
+		[Range(1,100,ErrorMessage ="Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+        //      public Category()
+        //{
+        //}
+        // suggests that the Name property in your Category class should be declared
+		// as nullable. To resolve the warning, you can update the property
+		// declaration to make it nullable by adding a ? after the property type. 
+    }
+}
+
